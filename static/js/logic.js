958ca6ca02e35d44d9c4d1da81a9e49c5904fa8f
Kevin Nazario <kevin.l.nazario@gmail.com>
2024-03-18 01:33:21 -0700

    Update map to include multiple years of EV data

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,17 +1,21 @@
 // 
-const path = 'data/all_stations_2014.01.31.geojson';
-const washington_2014 = 'data/washington_2014.01.31.geojson';
-const washington_2024 = 'data/washington_2024.01.31.geojson';
+const path = 'data/EV_Stations_All_US/all_stations_2014.01.31.geojson';
+const washington_2014 = 'data/EV_Stations_Washington/washington_2014.01.31.geojson';
+const washington_2024 = 'data/EV_Stations_Washington/washington_2024.01.31.geojson';
+const washington_2018 = 'data/EV_Stations_Washington/washington_2018.01.31.geojson';
+const washington_2022 = 'data/EV_Stations_Washington/washington_2022.01.31.geojson';
 
 const us_center = [38.5, -96.5];        // zoom level 5
 const wash_center = [47.4, -120.8];     // zoom level 
 
-jQuery.getJSON(washington_2024, function(data) {
-    // Add the GeoJSON layer to the map
-    addStations(data);
+jQuery.getJSON(washington_2018, function(data_2018) {
+    jQuery.getJSON(washington_2022, function(data_2022) {
+
+        createMap(data_2018, data_2022);
+    });
 });
 
-function addStations(data){
+function addStations(data, marker_color){
     console.log(data.features.length)
     console.log(data.features[0]);
 
@@ -24,11 +28,21 @@ function addStations(data){
             subset.push(station);
         }
     }
-    console.log(data.features[2]);
-    console.log(subset);
+    console.log(subset.length);
     let stations = L.geoJSON(subset, {
+        pointToLayer: createCircleMarker,
         onEachFeature: on_each_feature
-      });
+        });
+
+    function createCircleMarker(feature, latlng) {
+    return L.circleMarker(latlng, {
+        radius: 10,
+        fillColor: marker_color,
+        color: "#000", 
+        weight: .5,
+        fillOpacity: .5
+        });
+    }
 
     function on_each_feature(feature, layer) {
         layer.bindPopup(`<h3>${feature.properties.station_name}</h3><hr>`+
@@ -38,10 +52,10 @@ function addStations(data){
 
     let cluster = L.markerClusterGroup();
     stations.addTo(cluster)
-    createMap(cluster);
-    
+    //createMap(cluster);
+    return stations;
 }
-function createMap(markers){
+function createMap(data_2018, data_2022){
     // Create the tile layer (background) for map
     let base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -53,17 +67,19 @@ function createMap(markers){
     "Gray Scale": base
     };
 
-
+    markers_2018 = addStations(data_2018, 'red');
+    markers_2022 = addStations(data_2022, 'blue');
     // Create an overlayMaps object to hold the bikeStations layer.
     let overlayMaps = {
-    "EV Stations": markers
+    "EV Stations 2018": markers_2018,
+    "EV Stations 2022": markers_2022
     };
 
     // Create the map object with options.
     let myMap = L.map("map", {
     center: wash_center,
     zoom: 7,
-    layers: [base, markers]
+    layers: [base, markers_2018]
     });
 
     // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
